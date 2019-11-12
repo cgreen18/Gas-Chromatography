ad0277e12fe9ee75e1ac92cfc05cd844966dd550
Conor <cgreen18@lion.lmu.edu>
2019-11-11 20:18:31 -0800

    Update three_element_heat_control.ino

--- Triac Heating/Arduino/three_element_heat_control.ino
+++ Triac Heating/Arduino/three_element_heat_control.ino
@@ -6,19 +6,23 @@
  *  Usage: Upload to microcontroller with interrupt capability. Arduino Pro Micro 5V used.
  *  Version:
  *    1.0 - November 9 2019 - Initial creation.
+ *    2.0 - November 11 2019 - Moved to one zero crossing interrupt
  */
 
 // Constants - put in header/cpp later
 // Max error from: minimum temp ~ 30C & max temp desired ~ 180 & small margin
 float ERR_MAX = 160.0;
 
-float K_P_OVEN = 0.5;
-float K_I_OVEN = 0.2;
-float K_D_OVEN = 0.2;
+float K_P_OVEN = 0.98;
+float K_I_OVEN = 0.01;
+float K_D_OVEN = 0.01;
 
-float K_P_INJ_DET = 0.5;
-float K_I_INJ_DET = 0.2;
-float K_D_INJ_DET = 0.2;
+float K_P_INJ_DET = 0.98;
+float K_I_INJ_DET = 0.01;
+float K_D_INJ_DET = 0.01;
+
+// Decision making variables
+bool  x = true;
 
 // Oven pins
 int inter_oven = 2;
@@ -52,38 +56,138 @@ void setup() {
   
   pinMode(triac_inj_det,OUTPUT);
   pinMode(triac_oven,OUTPUT);
-  attachInterrupt(digitalPinToInterrupt(inter_oven), zc_inter_oven, RISING);
-  attachInterrupt(digitalPinToInterrupt(inter_inj_det), zc_inter_inj_det, RISING);
+  attachInterrupt(digitalPinToInterrupt(inter_oven), zc_interrupt, RISING);
+  //attachInterrupt(digitalPinToInterrupt(inter_inj_det), zc_inter_inj_det, RISING);
 }
 
+/*
 void zc_inter_oven(){
   //Serial.println(dim_oven);
-  dim_oven = 512;
+  dim_oven = 65*512;
   delayMicroseconds(dim_oven);
   digitalWrite(triac_oven,HIGH);
   delayMicroseconds(8.33);
   digitalWrite(triac_oven,LOW); 
 }
+*/
 
-void zc_inter_inj_det(){
-  delayMicroseconds(dim_inj_det);
-  digitalWrite(triac_inj_det,HIGH);
-  delayMicroseconds(8.33);
-  digitalWrite(triac_inj_det,LOW); 
+void zc_interrupt(){
+
+  dim_oven = 5000;
+  //dim_inj_det = (10000.0*/170);
+
+  // oven >= inj_det + 8.33
+  if(dim_oven >= (dim_inj_det + 8.33)){
+    float diff = dim_oven - (dim_inj_det + 8.33);
+
+    Serial.println("Case 1:");
+    Serial.println("Dim oven: ");
+    Serial.println(dim_oven);
+    Serial.println("Dim injector and detector");
+    Serial.println(dim_inj_det);
+    
+    delayMicroseconds(dim_inj_det);
+    digitalWrite(triac_inj_det,HIGH);
+    delayMicroseconds(8.33);
+    digitalWrite(triac_inj_det,LOW);
+    delayMicroseconds(diff);
+    digitalWrite(triac_oven,HIGH);
+    delayMicroseconds(8.33);
+    digitalWrite(triac_oven,LOW);
+  }
+  
+  // inj_det + 8.33 >= oven > inj_det
+  else if(dim_oven >= dim_inj_det){
+    float diff = dim_oven - dim_inj_det;
+
+    Serial.println(diff);
+    
+    Serial.println("Case 2:");
+    Serial.println("Dim oven: ");
+    Serial.println(dim_oven);
+    Serial.println("Dim injector and detector");
+    Serial.println(dim_inj_det);
+
+    delayMicroseconds(dim_inj_det);
+    digitalWrite(triac_inj_det,HIGH);
+    delayMicroseconds(diff);
+    digitalWrite(triac_oven,HIGH);
+    delayMicroseconds(8.33-diff);
+    digitalWrite(triac_inj_det,LOW);
+    delayMicroseconds(diff);
+    digitalWrite(triac_oven,LOW);
+  }
+
+  // inj_det >= oven +8.33
+  else if(dim_inj_det >= (dim_oven + 8.33)){
+    float diff = dim_inj_det - (dim_oven + 8.33);
+
+    Serial.println("Case 3:");
+    Serial.println("Dim oven: ");
+    Serial.println(dim_oven);
+    Serial.println("Dim injector and detector");
+    Serial.println(dim_inj_det);
+    Serial.println("Diff: ");
+    Serial.println(diff);
+
+    delayMicroseconds(dim_oven);
+    digitalWrite(triac_oven,HIGH);
+    delayMicroseconds(8.33);
+    digitalWrite(triac_oven,LOW);
+    delayMicroseconds(diff);
+    digitalWrite(triac_inj_det,HIGH);
+    delayMicroseconds(8.33);
+    digitalWrite(triac_inj_det,LOW);    
+  }
+
+  // oven + 8.33 > inj_det >= oven 
+  else if( dim_inj_det >= dim_oven){
+    float diff = dim_inj_det - dim_oven;
+  
+    Serial.println("Case 4:");
+    Serial.println("Dim oven: ");
+    Serial.println(dim_oven);
+    Serial.println("Dim injector and detector");
+    Serial.println(dim_inj_det);
+    
+    delayMicroseconds(dim_oven);
+    digitalWrite(triac_oven,HIGH);
+    delayMicroseconds(8.33-diff);
+    digitalWrite(triac_inj_det,HIGH);
+    delayMicroseconds(diff);
+    digitalWrite(triac_oven,LOW);
+    delayMicroseconds(8.33-diff);
+    digitalWrite(triac_inj_det,LOW); 
+  }
+  
+   
 }
 
 void update_temperatures(){
   int raw;
   raw = analogRead(thermo_oven);
   temp_oven = .284667*raw + 39.2;
-  Serial.print("Temp: ");
-  Serial.println(temp_oven);
-
+  if(x){
+    Serial.print("Oven temp: ");
+    Serial.println(temp_oven);
+  }
   raw = analogRead(thermo_inj);
   temp_inj = .284667*raw + 39.2;
 
+  if(x){
+    Serial.print("Injector temp: ");
+    Serial.println(temp_inj);
+  }
+
   raw = analogRead(thermo_det);
   temp_det = .284667*raw + 39.2;
+
+  if(x){
+    Serial.print("Detector temp: ");
+    Serial.println(temp_det);
+  }
+
+  x = !x;
 }
 
 void update_dimming(){
@@ -92,29 +196,50 @@ void update_dimming(){
   // Oven
   err = targ_oven - temp_oven;
 
-  Serial.print("Error:");
-  Serial.println(err);
+  
   p = K_P_OVEN*err;
   i = K_I_OVEN*err;
   d = K_D_OVEN*err;
 
   u = p + i + d;
-  Serial.print("u:");
-  Serial.println(u);
-  dim_oven = (int) ((35.0*1024.0/ERR_MAX)*(ERR_MAX-err));
-  Serial.println(dim_oven);
+  
+  dim_oven = (int) ((10.0*1024.0/ERR_MAX)*(ERR_MAX-err));
 
+  if(dim_oven >= 8000){
+    dim_oven = 8000;
+  }
+
+  /*
+  Serial.print("Oven Error: ");
+  Serial.println(err);
+  Serial.print("u: ");
+  Serial.println(u);
+  Serial.println("Oven microsecond delay: ")
+  Serial.println(dim_oven);
+  */
 
   // Injector and detector
   // Weighted average between injector and detector
-  err = targ_inj_det - 0.2*temp_det - 0.8*temp_inj;
+  err = targ_inj_det - (0.2*temp_det + 0.8*temp_inj);
 
   p = K_P_OVEN*err;
   i = K_I_OVEN*err;
   d = K_D_OVEN*err;
 
   u = p + i + d;
-  dim_inj_det = (int) ((65.0*1024.0/ERR_MAX)*(ERR_MAX-err));
+  dim_inj_det = (int) ((10.0*1024.0/ERR_MAX)*(ERR_MAX-err));
+
+  if(dim_inj_det >= 8000){
+    dim_inj_det = 8000;
+  }
+
+  Serial.print("Inj_det Error: ");
+  Serial.println(err);
+  Serial.print("u: ");
+  Serial.println(u);
+  Serial.println("Injector and detector microsecond delay: ");
+  Serial.println(dim_inj_det);
+  
 }
 
 
