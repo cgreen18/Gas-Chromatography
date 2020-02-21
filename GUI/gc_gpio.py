aa233dcab74e23b319a2b7975263724f2861d93f
cgreen18 <cgreen18@lion.lmu.edu>
2020-02-21 10:59:03 -0800

    slower loop

--- GUI/gc_gpio.py
+++ GUI/gc_gpio.py
@@ -1,3 +1,4 @@
+
 '''
 Name: gc_gpio.py
 Authors: Conor Green and Matt McPartlan
@@ -5,6 +6,7 @@ Description: Script to read analog input via ADS1115 A/D converter, as given in 
 Usage: Call as main
 Version:
 1.0 - 24 January 2020 - Initial creation. Serves as test bed to read voltage values from ADS1115. Will be integrated into larger script.
+1.1 - 18 February 2020 - Tested with a for loop in main. Found bug in defining bus
 '''
 
 import board
@@ -14,10 +16,19 @@ import adafruit_ads1x15.ads1115 as ADS
 
 from adafruit_ads1x15.analog_in import AnalogIn
 
+import time
+
 
 def main():
-    i2c = bus.I2C(board.SCL, board.SDA)
+    i2c = busio.I2C(board.SCL, board.SDA)
     ads = ADS.ADS1115(i2c)
+
+
+    chan = AnalogIn(ads,ADS.P0)
+
+    for i in range(0,1000):
+        print(chan.voltage)
+        time.sleep(.1)
 
 def single_ended():
     chan = AnalogIn(ads, ADS.P0)  #channel 1. For others use another pin
