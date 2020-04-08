7af015e9d874feb1c0eea8520e75bebbbc7d131a
cgreen18 <cgreen18@lion.lmu.edu>
2020-04-08 14:41:39 -0700

    changed to hardcoded 'pi' user

--- Installation/in2.sh
+++ Installation/in2.sh
@@ -1,6 +1,7 @@
+#!/bin/bash
 
 # Creating gc Py3 virtualenv
 cd ./home/pi/
 python3 -m venv .gc_venv
 # Enter virtualenv
-source "/home/$USER/.gc_venv/bin/activate"
+source "/home/pi/.gc_venv/bin/activate"
