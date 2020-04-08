8f57b16e787257a61fbbc3ee8e9f46374cde7e36
cgreen18 <cgreen18@lion.lmu.edu>
2020-04-08 14:47:03 -0700

    more

--- Installation/in3.sh
+++ Installation/in3.sh
@@ -8,5 +8,7 @@ echo "Installing dependencies"
 # # Upgrade pip
 # pip install -U pip
 
-# Install dependencies
-pip install -U six wheel setuptools
+# # Install dependencies
+# pip install -U six wheel setuptools
+
+apt-get install build-essential tk-dev libncurses5-dev libncursesw5-dev libreadline6-dev libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev
