63bd036cd80cf5bd5bc75c60e53200942f9751eb
cgreen18 <cgreen18@lion.lmu.edu>
2020-04-08 14:29:36 -0700

    fixed bugs

--- Installation/in1.sh
+++ Installation/in1.sh
@@ -3,6 +3,6 @@ echo "Installing and configuring virtualenv"
 # Installing virtualenv
 sudo apt-get install python3-venv
 sudo pip install virtualenv
-echo -e '\nexport PATH="/home/$USER/.local/bin:$PATH"' >> ~/.bashrc
-cd ~/
-source "./.bashrc"
+echo -e '\nexport PATH="/home/$USER/.local/bin:$PATH"' >> /home/pi/.bashrc
+echo -e '\nexport PATH="/home/pi/.local/bin:$PATH"' >> /home/pi/.bashrc
+source "/home/pi/.bashrc"
