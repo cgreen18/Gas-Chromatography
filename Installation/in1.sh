e39b88543373c77b6ede5fcedbb31bb3ae4b3cd0
cgreen18 <cgreen18@lion.lmu.edu>
2020-04-08 14:39:24 -0700

    label as bash script

--- Installation/in1.sh
+++ Installation/in1.sh
@@ -1,8 +1,10 @@
+#!/bin/bash
+
 echo "Installing and configuring virtualenv"
 
 # Installing virtualenv
 sudo apt-get install python3-venv
 sudo pip install virtualenv
 echo -e '\nexport PATH="/home/$USER/.local/bin:$PATH"' >> /home/pi/.bashrc
-echo -e '\nexport PATH="/home/pi/.local/bin:$PATH"' >> /home/pi/.bashrc
+echo '\nexport PATH="/home/pi/.local/bin:$PATH"' >> /home/pi/.bashrc
 source "/home/pi/.bashrc"
