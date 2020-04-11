8dadb5295b74a57bccd7ba99876c723d2e9704a4
Conor <cgreen18@lion.lmu.edu>
2020-04-10 23:27:52 -0700

    pretty printf

--- Installation/config.sh
+++ Installation/config.sh
@@ -1,11 +1,39 @@
 # !/bin/bash
 # config.sh
+
+printf "%-120s\n" "-------------------------------------------------------------------------------------------------------------------"
+printf "%-120s\n" "| Configuration script to install the required dependencies for Gas-Chromatography on a new Raspberry Pi."
+#printf "%-120s\n" "| Instructions given at https://github.com/cgreen18/Gas-Chromatography/tree/master/"
+printf "%-120s\n" "| Tested on Raspberry Pi Model 3B+ w/ Raspbian 10 Buster"
+printf "%-120s\n" "| Functions as intended as of 10 April 2020"
+printf "%-120s\n" "| Conor Green and Matt McPartlan"
+printf "%-120s\n" "-------------------------------------------------------------------------------------------------------------------"
+
+sleep 10s
+
+printf "\n\n\n\n\n\n\n\n\n"
+
+# Root privileges check
+if (( $EUID != 0 )); then
+    printf "Please run as root (i.e. sudo ./install.sh)\n"
+    exit
+fi
 # to configure auto-start programs and allow SPI/SCI
-
+printf "\n ""Enabling Serial in raspi-config"
 # Enable SPI and SCI from raspi-config
-# DOUBLE CHECK 1 IS CORRECT LATER
-sudo raspi-config nonint do_serial 1
-sudo raspi-config nonint do_serial 1
+raspi-config nonint do_serial 0
+cat /boot/cmdline.txt
 
+printf "Enabling SCI and SPI in raspi-config\n"
+# Enable SPI and SCI from raspi-config
+raspi-config nonint do_i2c 0
+raspi-config nonint do_spi 0
+
+printf "\nConfiguring 'gas_chromatograph.py' to autorun at startup via .bashrc\n"
 # Default gc_venv
-echo -e '\nsource "/home/$USER/.gc_venv/bin/activate"' >> ~/.bashrc
+echo -e '\n#Hokey to have 5 lines in .bashrc but this RPi is dedicated to GC, so oh well.' >> /home/pi/.bashrc
+echo -e '\nsource "/home/pi/.gc_venv/bin/activate"' >> /home/pi/.bashrc
+echo -e '\ncd /home/pi/Gas-Chromatography' >> /home/pi/.bashrc
+echo -e '\ncat README.md' >> /home/pi/.bashrc
+echo -e '\ncd /home/pi/Gas-Chromatography/GUI' >> /home/pi/.bashrc
+echo -e '\npython3 gas_chromatography.py' >> /home/pi/.bashrc
