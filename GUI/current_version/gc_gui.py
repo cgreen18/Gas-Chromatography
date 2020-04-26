836b539c3b09affbce13321fbe9c252ccf6d735c
cgreen18 <cgreen18@lion.lmu.edu>
2020-04-25 19:30:29 -0700

    format

--- GUI/current_version/gc_gui.py
+++ GUI/current_version/gc_gui.py
@@ -1328,15 +1328,12 @@ class DetectorPanel(wx.Panel):
             max_index , max_val  = maximas[i]
             _x = t[max_index]
             _y = v[max_index]
-            self.axes.annotate(_text, xy= (_x,_y), xytext=(10,10), xycoords='data', textcoords = 'offset pixels')
+            self.axes.annotate(_text, xy= (_x,_y), xytext=(10,-10), xycoords='data', textcoords = 'offset pixels')
 
         _xstr = self.units_str['x-axis']
         _ystr = self.units_str['y-axis']
         self.axes.set_xlabel(_xstr)
         self.axes.set_ylabel(_ystr)
-
-
-        self.axes.annotate('waddup b', xy = (_x, _y))
 
         func = self.canvas.draw
         wx.CallAfter(func)
